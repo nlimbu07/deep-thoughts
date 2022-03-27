91e08a9570c3c6999d6b95aee9606306f3198400
Niraj Limbu <nlimbu@yahoo.com>
2022-03-26 22:30:11 -0400

    implement user-based mutations

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import ReactionList from '../components/ReactionList';
-
+import Auth from '../utils/auth';
 import { useQuery } from '@apollo/client';
 import { QUERY_THOUGHT } from '../utils/queries';
+import ReactionForm from '../components/ReactionForm';
 
 const SingleThought = (props) => {
   const { id: thoughtId } = useParams();
@@ -36,6 +37,7 @@ const SingleThought = (props) => {
       {thought.reactionCount > 0 && (
         <ReactionList reactions={thought.reactions} />
       )}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
     </div>
   );
 };
